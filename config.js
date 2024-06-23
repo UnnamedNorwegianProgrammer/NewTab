e15e8152dcbffeb57bfa114fa117818956cb3c92
UnnamedNorwegianProgrammer <132568686+UnnamedNorwegianProgrammer@users.noreply.github.com>
2024-06-23 12:58:06 -0700

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ const CONFIG = {
 	greetingNight: 'Go to Sleep!',
 
 	// Layout
-	bentoLayout: 'buttons', // 'bento', 'lists', 'buttons'
+	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
 	weatherKey: 'jaslo-1957079', // Write here your API Key
